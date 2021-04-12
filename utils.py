71599016e2c507239c7c0f876c2b04990d1a4d36
Dhawal Darji <dhawal.d7@gmail.com>
2021-04-11 20:38:25 -0400

    Modified utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def checkIfRegPresent(traceLine):
-    registers = ["EAX", "EBX", "ECX", "EDX"]
+    '''Checks if any of the registers is present in the instruction line or not'''
+    registers = ["EAX", "EBX", "ECX", "EDX"] # all available registers
     check = False
-    traceLineSplit = traceLine.split(" ")
+    traceLineSplit = traceLine.split(" ")   # split trace line into tokens
     check = any(r == traceLineSplit[7].strip("\n") for r in registers)
     return check
