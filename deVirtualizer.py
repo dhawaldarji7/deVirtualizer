d33d3d980f134d943d9fa77c7714ba2df6e6ad52
Dhawal Darji <dhawal.d7@gmail.com>
2021-04-11 20:58:49 -0400

    Moving reusable code to methods in utils.py

--- deVirtualizer.py
+++ deVirtualizer.py
@@ -29,45 +29,7 @@ except FileNotFoundError:
 # Get the total length of the trace
 trace_len = len(trace)
 
-vm_start = vm_end = ""
-vm_start_lineno = vm_end_lineno = 0
-startFound = endFound = False
-vmCount, vmList = 0, []
-
-# Look up the trace for Virtual Machine's
-print("\nLength of original trace:", trace_len)
-print("\nSearching the trace for VMs...!")
-for i in range(0, trace_len):
-
-    # Fish-White VMs start with a JMP instruction followed by PUSHFD instruction.
-    if "JMP" in trace[i] and "PUSHFD" in trace[i+1]:
-        vm_start = trace[i+1].split()[0]
-        vm_start_lineno = i
-        startFound = True
-
-    # Fish-White VMs end with a POPFD followed by a RETN instruction.
-    if "POPFD" in trace[i] and "RETN 0" in trace[i+1]:
-        vm_end = trace[i+1].split()[0]
-        vm_end_lineno = i
-        endFound = True
-
-    # If a VM is found, store it in a list with the 'start' and 'end' address.
-    # Also write the VM into a different file with name as VM_START_END.txt
-    # These files will be used to analyze the VM once we've detected all the VMs
-    if startFound and endFound:
-        startFound = False
-        endFound = False
-        vmList.append([vm_start, vm_end])
-        vmCount += 1
-
-        # Create a separate trace file for each VM. File Format VM_startAddress_endAddress.txt
-        vm_file = "VM_" + vm_start + "_" + vm_end + ".txt"
-        writeVM = open(vm_file, "w")
-        for i in range(vm_start_lineno, vm_end_lineno + 1):
-            writeVM.write(trace[i])
-        vm_start_lineno = vm_end_lineno = 0
-        writeVM.close()
-print("VM search completed...!")
+vmList = u.searchVMs(trace, trace_len)
 
 # Analyzing all the VMs found
 if len(vmList):
@@ -78,25 +40,12 @@ if len(vmList):
 
     # Print the VM number along with its entry and exit address
     for vm in vmList:
-        print("\nVM number: {}  VM_ENTRY: {}  VM_EXIT: {}".format(i, vm[0], vm[1]))
+        u.printVMInfo(vm, i)
         i+=1
 
-        # Open trace of each VM. VM trace format VM_startAddress_endAddress.txt
-        vm_file = "VM_" + vm[0] + "_" + vm[1] + ".txt"
-        vmRead = open(vm_file, "r")
-        vmTrace = vmRead.readlines()
-        mainHandler = "MainHandler_" + "_" + vm[0] + ".txt"
-        mainHandlerWrite = open(mainHandler, "w")
-
-        # Printing the main handler of the VM
-        print("\nWriting the Main Handler of the VM.")
-        for line in vmTrace:
-            if(vmTrace.index(line) != 0):
-                # print(line.strip("\n"))
-                mainHandlerWrite.write(line)
-                if ("JMP" in line):
-                    break
-
+        vmTrace = u.readVMTrace(vm)
+        
+        u.getMainHandler(vm, vmTrace)
 
         # Analyze the VMs and search for dispatcher loops
         loops = []
--- utils.py
+++ utils.py
@@ -1,5 +1,70 @@
 #!/usr/bin/python3
 
+def searchVMs(trace, trace_len):
+
+    vm_start = vm_end = ""
+    vm_start_lineno = vm_end_lineno = 0
+    startFound = endFound = False
+    vmCount, vmList = 0, []
+
+    # Look up the trace for Virtual Machine's
+    print("\nLength of original trace:", trace_len)
+    print("\nSearching the trace for VMs...!")
+    for i in range(0, trace_len):
+
+        # Fish-White VMs start with a JMP instruction followed by PUSHFD instruction.
+        if "JMP" in trace[i] and "PUSHFD" in trace[i+1]:
+            vm_start = trace[i+1].split()[0]
+            vm_start_lineno = i
+            startFound = True
+
+        # Fish-White VMs end with a POPFD followed by a RETN instruction.
+        if "POPFD" in trace[i] and "RETN 0" in trace[i+1]:
+            vm_end = trace[i+1].split()[0]
+            vm_end_lineno = i
+            endFound = True
+
+        # If a VM is found, store it in a list with the 'start' and 'end' address.
+        # Also write the VM into a different file with name as VM_START_END.txt
+        # These files will be used to analyze the VM once we've detected all the VMs
+        if startFound and endFound:
+            startFound = False
+            endFound = False
+            vmList.append([vm_start, vm_end])
+            vmCount += 1
+
+            # Create a separate trace file for each VM. File Format VM_startAddress_endAddress.txt
+            vm_file = "VM_" + vm_start + "_" + vm_end + ".txt"
+            writeVM = open(vm_file, "w")
+            for i in range(vm_start_lineno, vm_end_lineno + 1):
+                writeVM.write(trace[i])
+            vm_start_lineno = vm_end_lineno = 0
+            writeVM.close()
+    print("VM search completed...!")
+    return vmList
+
+def printVMInfo(vm, index):
+    print("\nVM number: {}  VM_ENTRY: {}  VM_EXIT: {}".format(index, vm[0], vm[1]))
+
+def readVMTrace(vm):
+    # Open trace of each VM. VM trace format VM_startAddress_endAddress.txt
+    vm_file = "VM_" + vm[0] + "_" + vm[1] + ".txt"
+    vmRead = open(vm_file, "r")
+    return vmRead.readlines()
+
+def getMainHandler(vm, vmTrace):
+    mainHandler = "MainHandler_" + "_" + vm[0] + ".txt"
+    mainHandlerWrite = open(mainHandler, "w")
+
+    # Writing the main handler of the VM
+    print("\nWriting the Main Handler of the VM.")
+    for line in vmTrace:
+        if(vmTrace.index(line) != 0):
+            # print(line.strip("\n"))
+            mainHandlerWrite.write(line)
+            if ("JMP" in line):
+                break
+
 def checkIfRegPresent(traceLine):
     '''Checks if any of the registers is present in the instruction line or not'''
     registers = ["EAX", "EBX", "ECX", "EDX"] # all available registers
